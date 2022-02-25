c76cdda3795fb454c395c977dd65981b91c08e2d
BertVanAcker <bert.van.acker@hotmail.nl>
2022-02-25 11:00:10 +0100

    Update run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -25,5 +25,5 @@ PARENT_DIRECTORY="${DIR%/*}"
 cd ${PARENT_DIRECTORY}
 
 # run all unit tests
-python -m pytest --import-mode=append ../tests/
+python -m pytest --import-mode=append tests/
 # python -m unittest discover
