afb03111eeb55a1bdcb6a79571085fd3d6fb8018
Bert Van Acker <bva.bmkr@gmail.com>
2022-02-25 07:26:51 +0100

    --pytest verifier.py added

---
+++ Tests/verifier.py
@@ -0,0 +1,7 @@
+# Verifier to verify the pytest environment is set correctly
+
+from unittest import TestCase
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
